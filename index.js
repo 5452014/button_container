7b8cf2b69fff94f44d8765dfd13b3e6ff79e07a0
Nadeem Shaikh <112391206+5452014@users.noreply.github.com>
2024-03-20 20:49:21 +0000

    added all codes

---
+++ index.js
@@ -0,0 +1,12 @@
+let nums=[1,2,3,6,9,8,7,4];
+const ids=[1,2,3,6,9,8,7,4];
+
+let btn5=document.querySelector('#btn5')
+
+btn5.addEventListener('click', function () {
+    btn_values.unshift(btn_values.pop());
+   for (let i = 0; i < id.length; i++) {
+    document.getElementById("btn"+id[i]).innerHTML=btn_values[i]
+    
+   }
+});
