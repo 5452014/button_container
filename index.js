c775c03203ce9787339eb19fcda6e281ae5d4277
Nadeem Shaikh <112391206+5452014@users.noreply.github.com>
2024-03-20 21:04:09 +0000

    resolved typo issues

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let nums=[1,2,3,6,9,8,7,4];
-const ids=[1,2,3,6,9,8,7,4];
+let btn_values = [1, 2, 3, 6, 9, 8, 7, 4];
+        let id = [1, 2, 3, 6, 9, 8, 7, 4];
+        let btn5 = document.querySelector('#btn5');
 
-let btn5=document.querySelector('#btn5')
 
-btn5.addEventListener('click', function () {
-    btn_values.unshift(nums.pop());
-   for (let i = 0; i < id.length; i++) {
-    document.getElementById("btn"+id[i]).innerHTML=nums[i]
-    
-   }
-});
+        btn5.addEventListener('click', function () {
+            btn_values.unshift(btn_values.pop());
+           for (let i = 0; i < id.length; i++) {
+            document.getElementById("btn"+id[i]).innerHTML=btn_values[i]
+            
+           }
+        });
