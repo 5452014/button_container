fb212c637d06a899f4ddc4d8a533bab3e37361bb
Nadeem Shaikh <112391206+5452014@users.noreply.github.com>
2024-03-20 20:59:57 +0000

    fixed issues incode

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const ids=[1,2,3,6,9,8,7,4];
 let btn5=document.querySelector('#btn5')
 
 btn5.addEventListener('click', function () {
-    btn_values.unshift(btn_values.pop());
+    btn_values.unshift(nums.pop());
    for (let i = 0; i < id.length; i++) {
-    document.getElementById("btn"+id[i]).innerHTML=btn_values[i]
+    document.getElementById("btn"+id[i]).innerHTML=nums[i]
     
    }
 });
